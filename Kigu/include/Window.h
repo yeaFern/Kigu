04aa31834e3156e61986f19298a96ef15a20f18d
yeaFern <54524320+yeaFern@users.noreply.github.com>
2019-10-09 15:10:22 +1000

    Only send a single WindowResizeEvent

--- Kigu/include/Window.h
+++ Kigu/include/Window.h
@@ -13,11 +13,18 @@ class Window
 private:
 	GLFWwindow* m_Window;
 	EventQueue& m_EventQueue;
+
+	int m_Width;
+	int m_Height;
+
+	bool m_WasResized = false;
+	
+	void OnResize(int newWidth, int newHeight);
 public:
 	Window(const std::string& title, EventQueue& eventQueue);
 	~Window();
 
-	void Update() const;
+	void Update();
 
 	bool ShouldClose() const;
 };
--- Kigu/src/Window.cpp
+++ Kigu/src/Window.cpp
@@ -3,15 +3,23 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+void Window::OnResize(int newWidth, int newHeight)
+{
+	m_WasResized = true;
+
+	m_Width = newWidth;
+	m_Height = newHeight;
+}
+
 Window::Window(const std::string& title, EventQueue& eventQueue)
-	: m_EventQueue(eventQueue)
+	: m_Width(1280), m_Height(720), m_EventQueue(eventQueue)
 {
 	if (!glfwInit())
 	{
 		KIGU_FATAL("Failed to initialize GLFW.");
 	}
 
-	m_Window = glfwCreateWindow(1280, 720, title.c_str(), nullptr, nullptr);
+	m_Window = glfwCreateWindow(m_Width, m_Height, title.c_str(), nullptr, nullptr);
 	if (!m_Window)
 	{
 		glfwTerminate();
@@ -27,10 +35,10 @@ Window::Window(const std::string& title, EventQueue& eventQueue)
 		KIGU_FATAL("Failed to initialize OpenGL.");
 	}
 
-	glfwSetWindowUserPointer(m_Window, &m_EventQueue);
+	glfwSetWindowUserPointer(m_Window, this);
 
 	glfwSetWindowSizeCallback(m_Window, [](GLFWwindow* window, int width, int height) {
-		(*((EventQueue*)glfwGetWindowUserPointer(window))).Add<WindowResizeEvent>(width, height);
+		((Window*)glfwGetWindowUserPointer(window))->OnResize(width, height);
 	});
 }
 
@@ -40,10 +48,16 @@ Window::~Window()
 	glfwTerminate();
 }
 
-void Window::Update() const
+void Window::Update()
 {
 	glfwSwapBuffers(m_Window);
 	glfwPollEvents();
+
+	if (m_WasResized)
+	{
+		m_WasResized = false;
+		m_EventQueue.Add<WindowResizeEvent>(m_Width, m_Height);
+	}
 }
 
 bool Window::ShouldClose() const
